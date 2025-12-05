b78be253125413c707c0c0e4a0deb6784f5b4bfc
John Nguyen <jnguyen2575@gmail.com>
2025-12-04 19:20:54 -0500

    secured vars

--- frontend/app/api/translate/route.ts
+++ frontend/app/api/translate/route.ts
@@ -15,9 +15,7 @@ export async function POST(req: NextRequest) {
   try {
     // Call your FastAPI backend instead of Hugging Face
     const response = await fetch(
-      // process.env.SMALL100_API_URL || "http://localhost:9090/translate",
-      // "http://44.222.231.176:9090/translate",
-      "https://rs1n7y2j6k.execute-api.us-east-1.amazonaws.com/translate",
+      process.env.SMALL100_API_URL!,
 
       {
         method: "POST",
